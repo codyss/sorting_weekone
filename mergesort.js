0752d7be988ad94b730c6f39033826a64bc992e7
codyss <cody.schwarz@gmail.com>
2016-01-14 18:29:46 -0500

    at 630

---
+++ mergesort.js
@@ -0,0 +1,21 @@
+function merge (array1, array2) {
+
+    var returnArray = [];
+    var counter = Math.max(array1.length, array2.length);
+
+    for (var i = 0; i < counter; i++) {
+          if (!array1[i] === undefined ) {
+
+          }
+        }
+
+    }
+
+    // for each array in args
+    // take each element from them,
+        // compare the two
+        // push the smaller one
+        // store the larger
+
+    return returnArray;
+}
